12af96bf185d107b58271b3612ee794726c8b57d
Albbert123 <albertcp2003@gmail.com>
2025-05-01 17:55:39 +0200

    fixes

--- backend/app/controllers/order_controller.py
+++ backend/app/controllers/order_controller.py
@@ -9,7 +9,6 @@ router = APIRouter()
 
 @router.post("/")
 async def create_order(order: Order, order_service: OrderService = Depends()):
-    print(order)
     order_service.create_order(order)
     return {"message": "Order created successfully"}
 
