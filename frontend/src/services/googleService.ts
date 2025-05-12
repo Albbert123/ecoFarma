d2f6001b96aaaeae9e4f749404b5cf1b85bab6f2
Albbert123 <albertcp2003@gmail.com>
2025-05-12 16:52:23 +0200

    fix google service

--- frontend/src/services/googleService.ts
+++ frontend/src/services/googleService.ts
@@ -1,6 +1,5 @@
 export const loginWithGoogle = () => {
   const params = new URLSearchParams(window.location.search);
   const returnTo = params.get("returnTo") || "/";
-  console.log("params.get('returnTo')", params.get("returnTo"));
   window.location.href = `http://localhost:8000/auth/google/login?returnTo=${encodeURIComponent(returnTo)}`;
 };
